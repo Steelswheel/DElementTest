fdfe00f8f2a7490ecc0f2f4cffa216a12c528334
Steelswheel <50166539+Steelswheel@users.noreply.github.com>
2020-02-10 02:31:48 +0500

    final

--- homepage/app/js/script.js
+++ homepage/app/js/script.js
@@ -1,8 +1,24 @@
 const button = document.querySelector('.work__button');
 const modal = document.querySelector('.modal');
+const popup = document.querySelector('.popup');
+const closer = popup.querySelector('.popup__close');
+const form = document.querySelector('.modal__form');
+const submit = form.querySelector('.form__button');
+const fields = form.querySelectorAll('.field');
+
+
+closer.onclick = (event) => {
+  modal.style.display = "none";
+}
 
 button.onclick = (event) => {
   modal.style.display = "block";
+  form.style.display = "block";
+  popup.style.display = 'none';
+
+  fields.forEach((item) => {
+      item.value = null;
+  })
 }
 
 window.onclick = (event) => {
@@ -11,20 +27,43 @@ window.onclick = (event) => {
     }
 }
 
-const form = document.querySelector('.form');
-const submit = form.querySelector('.form__button');
-const fields = form.querySelectorAll('.field');
-
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  let errors = document.querySelectorAll('.error');
+
+  errors.forEach((item) => {
+    item.remove();
+  });
+
   fields.forEach((item) => {
       item.value.trim().toString();
       if (!item.value) {
-        item.value = 'Please fill this field';
-        item.innerHTML.color = "red";
-        setTimeout(() => item.value = null, 2000);
+        let err = document.createElement('span');
+        err.className = 'error';
+        err.innerHTML = "Empty!"
+        err.style.color = 'red';
+        item.after(err);
+        setTimeout(() => err.remove(), 2000);
+      } else {
+          const data = [];
+          fields.forEach((item) => {
+            data.push(item.value);
+          });
+
+          let response = fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          }).then(response => response.json());
+
+          if (response) {
+            form.style.display = "none";
+            popup.style.display = "block";
+          }
       }
-    })
+    });
 });
--- homepage/dist/js/script.js
+++ homepage/dist/js/script.js
@@ -1,8 +1,24 @@
 const button = document.querySelector('.work__button');
 const modal = document.querySelector('.modal');
+const popup = document.querySelector('.popup');
+const closer = popup.querySelector('.popup__close');
+const form = document.querySelector('.modal__form');
+const submit = form.querySelector('.form__button');
+const fields = form.querySelectorAll('.field');
+
+
+closer.onclick = (event) => {
+  modal.style.display = "none";
+}
 
 button.onclick = (event) => {
   modal.style.display = "block";
+  form.style.display = "block";
+  popup.style.display = 'none';
+
+  fields.forEach((item) => {
+      item.value = null;
+  })
 }
 
 window.onclick = (event) => {
@@ -10,3 +26,44 @@ window.onclick = (event) => {
         modal.style.display = "none";
     }
 }
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let errors = document.querySelectorAll('.error');
+
+  errors.forEach((item) => {
+    item.remove();
+  });
+
+  fields.forEach((item) => {
+      item.value.trim().toString();
+      if (!item.value) {
+        let err = document.createElement('span');
+        err.className = 'error';
+        err.innerHTML = "Empty!"
+        err.style.color = 'red';
+        item.after(err);
+        setTimeout(() => err.remove(), 2000);
+      } else {
+          const data = [];
+          fields.forEach((item) => {
+            data.push(item.value);
+          });
+
+          let response = fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          }).then(response => response.json());
+
+          if (response) {
+            form.style.display = "none";
+            popup.style.display = "block";
+          }
+      }
+    });
+});
