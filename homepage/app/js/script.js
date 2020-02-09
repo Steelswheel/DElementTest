6972df22ca45451c6fb25b37e29653c28aa69223
Steelswheel <50166539+Steelswheel@users.noreply.github.com>
2020-02-09 23:59:32 +0500

    js

--- homepage/app/js/script.js
+++ homepage/app/js/script.js
@@ -10,3 +10,21 @@ window.onclick = (event) => {
         modal.style.display = "none";
     }
 }
+
+const form = document.querySelector('.form');
+const submit = form.querySelector('.form__button');
+const fields = form.querySelectorAll('.field');
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  fields.forEach((item) => {
+      item.value.trim().toString();
+      if (!item.value) {
+        item.value = 'Please fill this field';
+        item.innerHTML.color = "red";
+        setTimeout(() => item.value = null, 2000);
+      }
+    })
+});
